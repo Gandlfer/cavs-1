4f8b6b539238596454ac21021d2b53bddb02177a
CawFee <darryllee1999@gmail.com>
2024-10-03 14:29:50 -0500

    Nodemon setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ROSLIB from 'roslib';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +11,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+const ros = new ROSLIB.Ros({url : 'ws://localhost:9090'});
+ros.on('error', function(error) { console.log( error ); });
+ros.on('connection', function() { console.log('Connection made!'); });
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
