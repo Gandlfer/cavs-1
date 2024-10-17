37d847c415ece12aa8910aa3de738ed27fb5c406
AsianCawFee <darryllee1999@gmail.com>
2024-10-16 22:56:36 -0500

    Remove StrictMode

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //console.log(s)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
