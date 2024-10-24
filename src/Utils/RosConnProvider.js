4edccbd30d7ecb7a3e62e041329416f113b5f565
AsianCawFee <darryllee1999@gmail.com>
2024-10-24 15:30:23 -0500

    Rework roscon to use context and unify all message data from topics and pubRate

--- src/Utils/RosConnProvider.js
+++ src/Utils/RosConnProvider.js
@@ -12,24 +12,74 @@ export const RosProvider = ({ children }) => {
   const [isCon, setIsCon] = useState(false);
   const [error, setError] = useState(null);
   const [ros, setRos] = useState(null);
-  const rosRef = useRef();
+  const topicSubDataRef = useRef({});
+  const [refresh, setRefresh] = useState(false);
+  let temp = useRef(false);
+  const [load, setLoad] = useState(true);
   let rosConn;
+  let topicList = [
+    "/mavs_ros/image",
+    "/nature/global_path",
+    "/nature/local_path",
+    "/nature/occupancy_grid",
+  ];
   useEffect(() => {
     function connection() {
       setLoading(true);
       rosConn = new ROSLIB.Ros({
-        url: "ws://localhost:9090",
+        url: "ws://tirian-33.ddns.net:9090",
       });
       setRos(rosConn);
       setLoading(false);
     }
     connection();
+  }, []);
+  useEffect(() => {
+    if (ros) {
+      ros.getTopics((result) => {
+        result.topics.forEach((topicName, i) => {
+          if (
+            topicName == "/mavs_ros/image" ||
+            topicName == "/nature/global_path" ||
+            topicName == "/nature/local_path" ||
+            topicName == "/nature/occupancy_grid"
+          ) {
+            const testTopic = new ROSLIB.Topic({
+              ros: ros,
+              name: topicName,
+              messageType: result["types"][i],
+            });
+
+            testTopic.subscribe((message) => {
+              if (!(topicName in topicSubDataRef.current)) {
+                topicSubDataRef.current[topicName] = {};
+                topicSubDataRef.current[topicName].prevTime = Date.now();
+                topicSubDataRef.current[topicName].pubRate = 0;
+              } else {
+                //console.log("CalcPubRate" + topicName);
+                topicSubDataRef.current[topicName].pubRate = Math.ceil(
+                  1 /
+                    ((Date.now() -
+                      topicSubDataRef.current[topicName].prevTime) /
+                      1000)
+                );
+                topicSubDataRef.current[topicName].prevTime = Date.now();
+              }
+              topicSubDataRef.current[topicName].message = message;
+              temp = !temp;
+              setRefresh(temp);
+            });
+          }
+        });
+      });
+    }
     return () => {
       if (ros != null) {
+        topicSubDataRef.current.keys().map((topicName, i) => {});
         ros.close();
       }
     };
-  }, []);
+  }, [ros]);
   if (ros != null) {
     ros.on("error", function (error) {
       console.log("Connection Error" + error);
@@ -46,15 +96,8 @@ export const RosProvider = ({ children }) => {
     });
   }
   return (
-    <RosContext.Provider value={{ ros, isCon }}>{children}</RosContext.Provider>
-    // <div>
-    //   <p>
-    //     {loading ? "Trying to Connect" : null}
-    //     {rosConn ? "Connected to Ros" : "Disconnected"}
-    //   </p>
-    //   {rosConn ? <Camera ros={ros} /> : null}
-    //   {/* {rosConn ? <Test ros={ros} /> : null}
-    //     {rosConn ? <IMU ros={ros} /> : null} */}
-    // </div>
+    <RosContext.Provider value={{ ros, isCon, topicSubDataRef, refresh }}>
+      {children}
+    </RosContext.Provider>
   );
 };
