d150dc84fc32917cf96fd3058d2f53a86ef5134e
AsianCawFee <darryllee1999@gmail.com>
2024-10-28 01:31:07 -0500

    Setup for MapComponent

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -6,30 +6,41 @@ import {
   Popup,
   Polyline,
 } from "react-leaflet";
+import { useRos } from "../Utils/RosConnProvider.js";
+import { useEffect } from "react";
+
 const MapComponent = () => {
+  const { ros, isCon, topicSubDataRef, refresh } = useRos();
+  useEffect(() => {
+    return;
+  }, [ros, isCon, refresh]);
   return (
-    <MapContainer
-      center={[33.453892, -88.788887]}
-      zoom={17}
-      scrollWheelZoom={true}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[33.453892, -88.788887]}>
-        <Popup>
-          Start point <br /> Mississippi State University
-        </Popup>
-      </Marker>
-      <Marker position={[33.452, -88.7884]}>
-        <Popup>
-          Destination <br /> Mississippi State University
-        </Popup>
-      </Marker>
-      <Polyline pathOption={{ color: "red" }} positions={polyline1} />
-    </MapContainer>
+    <>
+      {!isCon ? null : (
+        <MapContainer
+          center={[33.453892, -88.788887]}
+          zoom={17}
+          scrollWheelZoom={true}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[33.453892, -88.788887]}>
+            <Popup>
+              Start point <br /> Mississippi State University
+            </Popup>
+          </Marker>
+          <Marker position={[33.452, -88.7884]}>
+            <Popup>
+              Destination <br /> Mississippi State University
+            </Popup>
+          </Marker>
+          <Polyline pathOption={{ color: "red" }} positions={polyline1} />
+        </MapContainer>
+      )}
+    </>
   );
 };
 export default MapComponent;
