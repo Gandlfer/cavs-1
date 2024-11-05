913122970696da63a4a2a360a8a526930fb6b94a
AsianCawFee <darryllee1999@gmail.com>
2024-11-05 12:20:18 -0600

    Test point cloud

--- src/Components/PointCloud.js
+++ src/Components/PointCloud.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { useRos } from "../Utils/RosConnProvider.js";
-import { Grid, PointCloud2, Viewer } from "ros3d";
+import { Grid, PointCloud2, Viewer, ROS3D } from "ros3d";
 import ROSLIB from "roslib";
-import { point } from "leaflet";
+import DeckGL, { PointCloudLayer } from "deck.gl";
 
 const PointCloud = () => {
   const { ros, isCon, refresh, topicSubDataRef } = useRos();
   const [exist, setExist] = useState(false);
   const topicName = "/nature/points";
+  var layers;
 
   useEffect(() => {
     // ros.getTopicType(
@@ -22,54 +23,69 @@ const PointCloud = () => {
 
     if (
       isCon &&
-      !exist &&
-      document.getElementById("viewer") != null &&
+      // !exist &&
+      // document.getElementById("viewer") != null &&
       topicName in topicSubDataRef.current
     ) {
-      console.log(topicSubDataRef.current[topicName].message);
+      //console.log(topicSubDataRef.current[topicName].message);
+      //console.log("Here");
+      layers = new PointCloudLayer({
+        id: "PointCloudLayer",
+        data: parsePointCloud2Data(topicSubDataRef.current[topicName].message),
+        getPosition: (d) => {
+          d.position;
+        },
+        getColor: (d) => d.color,
+        pointSize: 1,
+        coordinateOrigin: [-122.4, 37.74],
+        pickable: true,
+      });
       //console.log(document.getElementById("viewer"));
-      setExist(true);
-      var viewer = new Viewer({
-        divID: "viewer",
-        width: 800,
-        height: 600,
-        antialias: true,
-      });
-      var tfClient = new ROSLIB.TFClient({
-        ros: ros,
-        angularThres: 0.01,
-        transThres: 0.01,
-        rate: 10.0,
-      });
-      var pointCloud = new PointCloud2({
-        ros: ros,
-        topic: topicName,
-        tfClient: tfClient,
-        rootObject: viewer.scene,
-        material: { size: 0.05, color: 0xff00ff },
-      });
-      viewer.addObject(pointCloud);
-      pointCloud.processMessage(topicSubDataRef.current[topicName].message);
-
-      //viewer.addObject();
-      viewer.addObject(new Grid());
-
-      //   var tfClient = new ROSLIB.TFClient({
-      //     ros : ros,
-      //     angularThres : 0.01,
-      //     transThres : 0.01,
-      //     rate : 10.0,
-      //     fixedFrame : '/camera_link'
-      //   });
-      //   console.log(
-      //     parsePointCloud2Data(topicSubDataRef.current[topicName].message)
-      //   );
+      //setExist(true);
+      // var viewer = new Viewer({
+      //   divID: "viewer",
+      //   width: 800,
+      //   height: 600,
+      //   antialias: true,
+      // });
+      // var tfClient = new ROSLIB.TFClient({
+      //   ros: ros,
+      //   angularThres: 0.01,
+      //   transThres: 0.01,
+      //   rate: 10.0,
+      // });
+      // var pointCloud = new PointCloud2({
+      //   ros: ros,
+      //   topic: topicName,
+      //   tfClient: tfClient,
+      //   rootObject: viewer.scene,
+      //   material: { size: 1000, color: 0xffffff },
+      //   max_pts: 10000,
+      //   points: "test",
+      // });
+      // viewer.addObject(pointCloud);
+      // pointCloud.processMessage(topicSubDataRef.current[topicName].message);
+      // //viewer.addObject();
+      // viewer.addObject(new Grid());
     }
   }, [ros, isCon, refresh]);
   return (
+    // <div className="card">
+    //   <h3 className="card-title"> Lidar </h3>
+    //   <div id="viewer"></div>
+    // </div>
     <div className="card">
-      <h3 className="card-title"> Lidar </h3>
-      <div id="viewer"></div>
+      <DeckGL
+        initialViewState={{
+          longitude: -122.45,
+          latitude: 37.78,
+          zoom: 11,
+        }}
+        controller
+        //getTooltip={({object}: PickingInfo<DataType>) => object && object.position.join(', ')}
+        layers={[layers]}
+        //style={{ backgroundColor: "black" }} // Set background color to black
+      />
     </div>
   );
 };
@@ -95,12 +111,15 @@ function parsePointCloud2Data(message) {
         }
         // Add other datatype conversions if needed
       });
-
-      points.push(point);
       //console.log(point);
+      points.push({
+        position: [point.x, point.y, point.z],
+        color: [0, 0, point.intensity],
+      });
+      //console.log(points);
     }
   }
-
+  //console.log(points);
   return points;
 }
 function base64ToUint8Array(base64) {
