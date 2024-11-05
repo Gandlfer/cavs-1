7012daf798c989cd0f59078d7deb993c4b3bbe0f
AsianCawFee <darryllee1999@gmail.com>
2024-11-05 12:19:43 -0600

    Implement IMU

--- src/Components/IMU.js
+++ src/Components/IMU.js
@@ -1,46 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ROSLIB from "roslib";
+import * as THREE from "three";
+import { useRos } from "../Utils/RosConnProvider.js";
 
-const IMU = (props) => {
-  const name = "/nature/points";
+const IMU = () => {
+  const { ros, isCon, refresh, topicSubDataRef } = useRos();
+  const mountRef = useRef(null);
+  const sceneRef = useRef(new THREE.Scene());
+  const cameraRef = useRef(
+    new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+  );
+  const rendererRef = useRef(new THREE.WebGLRenderer({ antialias: true }));
+  const cubeRef = useRef(null);
+  const topicName = "/mavs_ros/imu";
   const [data, setData] = useState(0);
   useEffect(() => {
-    let topicType;
-    //Function to get the topic type
-    props.ros.getTopicType(
-      name,
-      (type) => {
-        console.log(name + " is type ");
-        console.log(type);
-        topicType = type;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-    //Initialize a topic to subscribe to
-    const testTopic = new ROSLIB.Topic({
-      ros: props.ros,
-      name: name,
-      messageType: topicType,
-    });
-    //subscribe to the topic and update the data
-    testTopic.subscribe((message) => {
-      console.log(message);
-      setData(message.height);
-    });
+    //console.log(topicSubDataRef.current[topicName]);
+    rendererRef.current.setSize(window.innerWidth, window.innerHeight);
+    mountRef.current.appendChild(rendererRef.current.domElement);
+    cameraRef.current.position.z = 5;
 
-    return () => {
-      testTopic.unsubscribe();
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: true,
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    sceneRef.current.add(cube);
+    cubeRef.current = cube;
+
+    return () => {};
+  });
+  useEffect(() => {
+    const animate = () => {
+      requestAnimationFrame(animate);
+      rendererRef.current.render(sceneRef.current, cameraRef.current);
     };
+    animate();
   }, []);
-
+  useEffect(() => {
+    if (isCon && topicName in topicSubDataRef.current && cubeRef.current) {
+      console.log(topicSubDataRef.current[topicName].message);
+      const { x, y, z, w } =
+        topicSubDataRef.current[topicName].message.orientation; // Extract quaternion data from imuData
+      const quaternion = new THREE.Quaternion(x, y, z, w);
+      cubeRef.current.quaternion.copy(quaternion); // Apply orientation to the cube
+    }
+  }, [[ros, isCon, refresh]]);
   return (
-    <div>
-      <p>Current</p>
-      {data}
+    <div className="card" id="IMU-card">
+      <h3 className="card-title"> IMU </h3>
+      <div ref={mountRef} />
     </div>
   );
 };
-
 export default IMU;
--- src/routes/Sensor.jsx
+++ src/routes/Sensor.jsx
@@ -3,13 +3,21 @@ import Card from "../Components/Card";
 import Camera from "../Components/Camera";
 import { RosProvider, useRos } from "../Utils/RosConnProvider";
 import PointCloud from "../Components/PointCloud";
+import IMU from "../Components/IMU";
 
 export default function Sensor() {
   const { isCon } = useRos();
 
   return (
     <div id="sensor-tab" className="body">
-      {isCon ? <Camera /> : <Card />}
+      {isCon ? <Camera /> : <div className="card">
+          <h3 className="card-title"> Camera </h3>
+          <img
+            className="sample-img"
+            src="https://cdn.discordapp.com/attachments/1281269784415309956/1301304999296503909/image.png?ex=6723fe38&is=6722acb8&hm=2581bbd0e2f7da664dd50e680d473d7216046b9fb4de029836d4b9d8f809669c&"
+            alt="lidar"
+          />
+        </div>}
       {isCon ? (
         <PointCloud />
       ) : (
@@ -31,6 +39,7 @@ export default function Sensor() {
           alt="lidar sensor fusion"
         />
       </div>
+      {isCon ? <IMU/> :
       <div className="card" id="IMU-card">
         <h3 className="card-title"> IMU </h3>
         <img
@@ -39,6 +48,7 @@ export default function Sensor() {
           alt="IMU visualization"
         />
       </div>
+      }
     </div>
   );
 }
