488b96d926a0a6a95beba51a7ef50a0cb0d1a421
AsianCawFee <darryllee1999@gmail.com>
2024-11-25 13:51:07 -0600

    Making subscribed topic draggable

--- src/Components/TopicPublishRate.js
+++ src/Components/TopicPublishRate.js
@@ -4,12 +4,48 @@ import { useRos } from "../Utils/RosConnProvider.js";
 
 const TopicPublishRate = () => {
   const { ros, isCon, topicSubDataRef, refresh } = useRos();
-  useEffect(() => {}, [refresh, ros, isCon]);
+  const [topics, setTopics] = useState([]);
+  const draggedIndex = useRef(null);
+
+  useEffect(() => {}, [ros, isCon]);
+  useEffect(() => {
+    const topicNames = Object.keys(topicSubDataRef.current);
+    setTopics((prevTopics) => {
+      const newTopics = topicNames.filter((name) => !prevTopics.includes(name));
+      return [...prevTopics, ...newTopics];
+    });
+  }, [refresh]);
+
+  const handleDragStart = (index) => {
+    draggedIndex.current = index;
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (index) => {
+    if (draggedIndex.current !== null) {
+      const updatedTopics = [...topics];
+      const [draggedItem] = updatedTopics.splice(draggedIndex.current, 1);
+      updatedTopics.splice(index, 0, draggedItem);
+      setTopics(updatedTopics);
+      draggedIndex.current = null;
+    }
+  };
+
   return (
     <div>
-      {Object.keys(topicSubDataRef.current).length > 0
-        ? Object.keys(topicSubDataRef.current).map((topic, index) => (
-            <li key={topic} className="topic">
+      {topics.length > 0
+        ? topics.map((topic, index) => (
+            <li
+              key={topic}
+              className="topic"
+              draggable
+              onDragStart={() => handleDragStart(index)}
+              onDragOver={handleDragOver}
+              onDrop={() => handleDrop(index)}
+            >
               <span
                 className="topic-name"
                 id={index % 2 == 0 ? "light" : "dark"}
