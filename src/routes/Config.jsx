ad3a20540ade6e56c6183026d2a97f89cf5214e9
KhoaNguyen <117057796+KoaHaz@users.noreply.github.com>
2024-11-26 15:17:40 -0600

    added server path save button

--- src/routes/Config.jsx
+++ src/routes/Config.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ConfigData from "../PlaceholderFiles/ConfigData";
 import ConfigTopicAvailable from "../Components/ConfigTopicAvailable";
+import * as Io5 from "react-icons/io5";
 
 export default function Config() {
   return (
     <div id="config-container" className="body">
       <div id="server-box" className="status-card">
-        <p className="topic-name"> Server Address</p>
+        <span id="server-name" className="topic-name">
+          Server Address
+        </span>
         <input
-          id="server-text"
+          id="server-path"
           className="topic-path"
           label="Server Address"
-          defaultValue={"Server address path"}
+          defaultValue={"Server path"}
         />
+        <button id="save-server-button" className="ctrl-button">
+          <Io5.IoSaveOutline />
+        </button>
       </div>
       <div className="status-card">
         <h3 className="card-title">Components</h3>
