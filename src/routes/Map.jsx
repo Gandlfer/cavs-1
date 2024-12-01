58c28cc232567f874e9c8abeb2c8432b442e7344
Tirian33 <tirianjudy@gmail.com>
2024-12-01 17:13:53 -0600

    Functional Lidar on Map page.

--- src/routes/Map.jsx
+++ src/routes/Map.jsx
@@ -1,53 +1,26 @@
 import React from "react";
+import PointCloud from "../Components/PointCloud";
 import Card from "../Components/Card";
 import "../index.css";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  Polyline,
-} from "react-leaflet";
 import "./Map.css";
-import { MdHeight } from "react-icons/md";
+import { useRos } from "../Utils/RosConnProvider";
 import MapComponent from "../Components/MapComponent";
 
 export default function Map() {
+  const { isCon } = useRos();
   return (
     <div id="map-tab" className="body">
       <div id="map" className="card">
-        {/* <MapContainer
-          center={[33.453892, -88.788887]}
-          zoom={17}
-          scrollWheelZoom={true}
-          style={{ height: "100%", width: "100%" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[33.453892, -88.788887]}>
-            <Popup>
-              Start point <br /> Mississippi State University
-            </Popup>
-          </Marker>
-          <Marker position={[33.452, -88.7884]}>
-            <Popup>
-              Destination <br /> Mississippi State University
-            </Popup>
-          </Marker>
-          <Polyline pathOption={{ color: "red" }} positions={polyline1} />
-        </MapContainer> */}
         <MapComponent />
       </div>
-      <div className="card">
-        <h3 className="card-title"> Lidar </h3>
-        <img
-          className="sample-img"
-          src="https://cdn.discordapp.com/attachments/1281269784415309956/1301304999296503909/image.png?ex=672de178&is=672c8ff8&hm=b4b2c86687aa161d59ff70cb54a579998afdb434f325913c32d5e259b9ee27f3&"
-          alt="lidar"
-        />
-      </div>
+      {isCon ? (
+        <PointCloud />
+      ) : (
+        <div className="card">
+          <h3 className="card-title"> Lidar </h3>
+          <p> No websocket connection.</p>
+        </div>
+      )}
       <div className="card">
         <h3 className="card-title"> Occupancy grid </h3>
         <img
